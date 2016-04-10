c3d97a54402182c3224c38b12a8676398592de63
Domas <domasx2@gmail.com>
2016-04-10 16:54:09 +0300

    enable ssl detection gunicorn/django

--- backend/gunicorn.conf.py
+++ backend/gunicorn.conf.py
@@ -6,5 +6,4 @@ errorlog = '/tmp/logs/gunicorn/access.log'
 loglevel = 'info'
 accesslog = '/tmp/logs/gunicorn/error.log'
 
-# UNCOMMENT BELOW IF USING SSL
-#secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
+secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
